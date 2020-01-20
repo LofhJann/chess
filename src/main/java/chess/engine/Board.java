85d750b0654d089eefab3c33c156f39eb0fb8346
Janne Löfhjelm <janne.lofhjelm@gmail.com>
2020-01-20 19:33:58 +0200

    Add Piece lists

--- src/main/java/chess/engine/Board.java
+++ src/main/java/chess/engine/Board.java
@@ -1,6 +1,14 @@
 package chess.engine;
 
+import chess.engine.piece.*;
+
 public class Board {
 
     private char[] bitBoard = new char[128];
+    private Piece[] blackPieces = new Piece[]{new Pawn(), new Pawn(), new Pawn(), new Pawn(), new Pawn(), new Pawn(),
+            new Pawn(), new Pawn(), new Rook(), new Rook(), new Bishop(), new Bishop(), new Queen(), new King(), new Knight(), new Knight()
+    };
+    private Piece[] whitePieces = new Piece[]{new Pawn(), new Pawn(), new Pawn(), new Pawn(), new Pawn(), new Pawn(),
+            new Pawn(), new Pawn(), new Rook(), new Rook(), new Bishop(), new Bishop(), new Queen(), new King(), new Knight(), new Knight()
+    };
 }
