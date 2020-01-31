71c118057a76054d2aade8654478d07099e9b868
Janne Löfhjelm <janne.lofhjelm@gmail.com>
2020-01-31 18:29:51 +0200

    Fix PairTest

--- src/test/java/data/PairTest.java
+++ src/test/java/data/PairTest.java
@@ -9,28 +9,28 @@ public class PairTest {
 
     @Test
     public void addOnlyAtoPairWorks() {
-        Pair<Object> testPair = new Pair<>();
+        Pair<Object, Object> testPair = new Pair<>();
         testPair.setA("hello");
         Assert.assertNotNull(testPair.getA());
     }
 
     @Test
     public void addOnlyBtoPairWorks() {
-        Pair<Object> testPair = new Pair<>();
+        Pair<Object, Object> testPair = new Pair<>();
         testPair.setB("hello");
         Assert.assertNotNull(testPair.getB());
     }
 
     @Test
     public void addBothToPairConstructorWorks() {
-        Pair<Object> testPair = new Pair<>("hello", "world");
+        Pair<Object, Object> testPair = new Pair<>("hello", "world");
         Assert.assertNotNull(testPair.getA());
         Assert.assertNotNull(testPair.getB());
     }
 
     @Test
     public void pairGenericsWorks() {
-        Pair<String> testPair = new Pair<>("hello", "world");
+        Pair<String, String> testPair = new Pair<>("hello", "world");
 
         Assert.assertThat(testPair.getA(), instanceOf(String.class));
         Assert.assertThat(testPair.getB(), instanceOf(String.class));
