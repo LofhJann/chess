2e8952428c35eb1b4df34b934d43767987be36b8
Janne Löfhjelm <janne.lofhjelm@gmail.com>
2020-01-31 18:11:02 +0200

    Add documentation

--- src/main/java/data/Pair.java
+++ src/main/java/data/Pair.java
@@ -2,34 +2,67 @@ package data;
 
 import java.util.StringJoiner;
 
+/**
+ * Simple class for Data Pairs
+ * @param <T> Object
+ * @param <E> Object
+ */
 public class Pair<T, E> {
     private T a;
     private E b;
 
+    /**
+     * Create Pair with parameters set
+     * @param a Object
+     * @param b Object
+     */
     public Pair(T a, E b) {
         this.a = a;
         this.b = b;
     }
 
+    /**
+     * Create Pair without initialized parameters
+     */
     public Pair() {
     }
 
+    /**
+     *
+     * @return Object set in A
+     */
     public T getA() {
         return a;
     }
 
+    /**
+     * Set parameter a
+     * @param a Object to set
+     */
     public void setA(T a) {
         this.a = a;
     }
 
+    /**
+     *
+     * @return Object set in B
+     */
     public E getB() {
         return b;
     }
 
+    /**
+     * Set Parameter b
+     * @param b Object to set
+     */
     public void setB(E b) {
         this.b = b;
     }
 
+    /**
+     * Returns String representation of Class
+     * @return String formatted "Pair[a, b]"
+     */
     @Override
     public String toString() {
         return new StringJoiner(", ", Pair.class.getSimpleName() + "[", "]")
