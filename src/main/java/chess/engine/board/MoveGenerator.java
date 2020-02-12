c43facb520f02f85b29e34993abf6a38027b8434
Janne Löfhjelm <janne.lofhjelm@gmail.com>
2020-02-12 17:30:41 +0200

    Add skeleton for MoveGenerator

---
+++ src/main/java/chess/engine/board/MoveGenerator.java
@@ -0,0 +1,43 @@
+package chess.engine.board;
+
+import data.ArrayList;
+
+public class MoveGenerator {
+
+    public static ArrayList<Move> generateLegalMoves(Board board) {
+
+        return new ArrayList<>();
+    }
+
+    private static ArrayList<Move> generatePseudoLegalMoves(Board board) {
+        ArrayList<Move> moves = new ArrayList<>();
+
+        return moves;
+    }
+
+    // TODO: enpassant
+    private static ArrayList<Move> generatePawnMoves(Board board, ArrayList<Move> moves) {
+        return moves;
+    }
+
+    private static ArrayList<Move> generateBishopMoves(Board board, ArrayList<Move> moves) {
+        return moves;
+    }
+
+    // TODO: Castling
+    private static ArrayList<Move> generateKingMoves(Board board, ArrayList<Move> moves) {
+        return moves;
+    }
+
+    private static ArrayList<Move> generateKnightMoves(Board board, ArrayList<Move> moves) {
+        return moves;
+    }
+
+    private static ArrayList<Move> generateQueenMoves(Board board, ArrayList<Move> moves) {
+        return moves;
+    }
+
+    private static ArrayList<Move> generateRookMoves(Board board, ArrayList<Move> moves) {
+        return moves;
+    }
+}
