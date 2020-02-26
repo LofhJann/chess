63331742c9e0799d17cbe18e8b87f5cd67263364
Janne Löfhjelm <janne.lofhjelm@gmail.com>
2020-02-26 15:30:48 +0200

    Skeleton for pseudolegal move generation

--- src/main/java/chess/engine/board/MoveGenerator.java
+++ src/main/java/chess/engine/board/MoveGenerator.java
@@ -1,6 +1,7 @@
 package chess.engine.board;
 
 import chess.engine.piece.Color;
+import chess.engine.piece.Direction;
 import chess.engine.piece.Piece;
 import data.ArrayList;
 
@@ -11,46 +12,28 @@ public class MoveGenerator {
         return new ArrayList<>();
     }
 
+    // TODO: Finish and test this
     private static ArrayList<Move> generatePseudoLegalMoves(Board board, Color currentPlayer) {
         ArrayList<Move> moves = new ArrayList<>();
 
-        generatePawnMoves(board, moves);
-        generateBishopMoves(board, moves);
-        generateKingMoves(board, moves);
-        generateKnightMoves(board, moves);
-        generateQueenMoves(board, moves);
-        generateRookMoves(board, moves);
+        for (Piece piece : getPieceList(board, currentPlayer)) {
+            for (Direction direction : piece.getMoveDirections()) {
+                int currentSquare = piece.getPosition();
 
-        return moves;
-    }
+                while (currentSquare > Square.A1.getIntValue() && currentSquare < Square.H8.getIntValue()) {
+                    int targetSquare = currentSquare + direction.intValue;
+                    if (piece.isPseudoLegalMove(targetSquare)) {
+                        moves.add(new Move(Square.getString(currentSquare), Square.getString(targetSquare)));
+                    }
+                    currentSquare = targetSquare;
+                }
+            }
+        }
+        // TODO: enpassant
+        // TODO: Castling
 
-    // TODO: enpassant
-    private static ArrayList<Move> generatePawnMoves(Board board, ArrayList<Move> moves) {
-        return moves;
-    }
 
-    private static ArrayList<Move> generatePawnCaptures(Board board, ArrayList<Move> moves) {
-        return moves;
-    }
 
-    private static ArrayList<Move> generateBishopMoves(Board board, ArrayList<Move> moves) {
-        return moves;
-    }
-
-    // TODO: Castling
-    private static ArrayList<Move> generateKingMoves(Board board, ArrayList<Move> moves) {
-        return moves;
-    }
-
-    private static ArrayList<Move> generateKnightMoves(Board board, ArrayList<Move> moves) {
-        return moves;
-    }
-
-    private static ArrayList<Move> generateQueenMoves(Board board, ArrayList<Move> moves) {
-        return moves;
-    }
-
-    private static ArrayList<Move> generateRookMoves(Board board, ArrayList<Move> moves) {
         return moves;
     }
 
