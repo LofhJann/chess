742c97b8c6f8fee7c804012d8256f3c6f45aa860
Janne Löfhjelm <janne.lofhjelm@gmail.com>
2020-03-03 23:32:51 +0200

    Add test for Movegenerator

---
+++ src/test/java/chess/engine/MoveGeneratorTest.java
@@ -0,0 +1,22 @@
+package chess.engine;
+
+import chess.engine.board.Board;
+import chess.engine.board.Move;
+import chess.engine.board.MoveGenerator;
+import chess.engine.piece.Color;
+import data.ArrayList;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoveGeneratorTest {
+
+
+    @Test
+    public void centeredQueenMovesGeneratedCorrectly() {
+        Board board = new Board("8/8/8/3q");
+
+        ArrayList<Move> moves = MoveGenerator.generateLegalMoves(board, Color.WHITE);
+
+        Assert.assertEquals(28, moves.size());
+    }
+}
