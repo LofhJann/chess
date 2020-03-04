b0fcd0874abd1d56f08b3214fe180ce5dce8a82e
Janne Löfhjelm <janne.lofhjelm@gmail.com>
2020-03-04 19:26:36 +0200

    Add String formatted constructor

--- src/main/java/chess/engine/board/Move.java
+++ src/main/java/chess/engine/board/Move.java
@@ -1,6 +1,6 @@
 package chess.engine.board;
 
-public class Move{
+public class Move {
 
     private Square startingSquare;
     private Square endSquare;
@@ -9,6 +9,12 @@ public class Move{
     public Move(Square startingSquare, Square endSquare) {
         this.startingSquare = startingSquare;
         this.endSquare = endSquare;
+    }
+
+    public Move(String moveString) {
+        this.startingSquare = Square.valueOf(moveString.substring(0, 2));
+        this.endSquare = Square.valueOf(moveString.substring(2, 4));
+        this.promotion = moveString.substring(4);
     }
 
     public Move(Square startingSquare, Square endSquare, String promotion) {
