19c1172c7555242c1c225a1a177093dc3a0c0ab3
Janne Löfhjelm <janne.lofhjelm@gmail.com>
2020-03-04 03:26:11 +0200

    Add check for pieces being blocked

--- src/main/java/chess/engine/board/MoveGenerator.java
+++ src/main/java/chess/engine/board/MoveGenerator.java
@@ -2,6 +2,7 @@ package chess.engine.board;
 
 import chess.engine.piece.Color;
 import chess.engine.piece.Direction;
+import chess.engine.piece.Pawn;
 import chess.engine.piece.Piece;
 import data.ArrayList;
 
@@ -27,8 +28,15 @@ public class MoveGenerator {
                      targetSquare >= Square.A1.getIntValue() && targetSquare <= Square.H8.getIntValue() && (targetSquare & 0x88) == 0;
                      targetSquare += direction.intValue) {
 
+
+                    if (!board.squareIsEmpty(targetSquare)) {
+                        if (board.getSquarePieceColor(targetSquare) != currentPlayer && piece.getClass() != Pawn.class) {
+                            addMove(moves, startingSquare, targetSquare);
+                        }
+                        break;
+                    }
                     if (piece.isPseudoLegalMove(targetSquare, direction)) {
-                        moves.add(new Move(Square.getString(startingSquare), Square.getString(targetSquare)));
+                        addMove(moves, startingSquare, targetSquare);
                     }
                 }
             }
@@ -38,6 +46,10 @@ public class MoveGenerator {
 
 
         return moves;
+    }
+
+    private static void addMove(ArrayList<Move> moves, int startingSquare, int targetSquare) {
+        moves.add(new Move(Square.getString(startingSquare), Square.getString(targetSquare)));
     }
 
     private static ArrayList<Piece> getPieceList(Board board, Color color) {
